42893fd98314ff1c2bedfe5eef608314d3924334
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-06-23 16:13:16 +0200

    🎨 M- improve firebase service

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,9 +12,10 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
