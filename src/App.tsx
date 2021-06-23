004ef256ce653c33efe013fa81dbbc0e8490bd7e
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-06-23 16:10:29 +0200

    🎨 M- added routes and context provider for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Button } from './components/Button';
+import { Home } from 'pages/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { NewRoom } from 'pages/NewRoom';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button />
-    </>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
