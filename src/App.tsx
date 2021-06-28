c1205608207b3dc65adef906624491f3b9b4f735
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-06-28 12:11:47 +0200

    🎨 M- create a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Home } from 'pages/Home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Room } from 'pages/Room';
 import { NewRoom } from 'pages/NewRoom';
+import { AdminRoom } from 'pages/AdminRoom';
+import { ToastContainer } from 'react-toastify';
 import { AuthContextProvider } from './contexts/AuthContext';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer autoClose={3000} />
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
