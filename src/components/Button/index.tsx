bf96eeec739659985199cd43bef278ca7241ceca
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-06-29 16:54:32 +0200

    🎨 M- added a condition to button color

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,20 @@ import './styles.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isOutlined?: boolean;
+  logoutButton?: boolean;
 };
 
-export function Button({ isOutlined = false, ...props }: ButtonProps) {
+export function Button({
+  isOutlined = false,
+  logoutButton = false,
+  ...props
+}: ButtonProps) {
   return (
-    <button className={`button ${isOutlined ? 'outlined' : ''} `} {...props} />
+    <button
+      className={`button ${isOutlined ? 'outlined' : ''} ${
+        logoutButton ? 'logout-button' : ''
+      } `}
+      {...props}
+    />
   );
 }
